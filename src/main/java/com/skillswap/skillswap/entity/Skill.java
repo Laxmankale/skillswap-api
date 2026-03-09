e24f1b4e174d605373b975c021ab44d894ee27bc
Laxman Kale <lakhankale888@gmail.com>
2026-03-09 16:11:42 +0530

    Add Skill entity

---
+++ src/main/java/com/skillswap/skillswap/entity/Skill.java
@@ -0,0 +1,40 @@
+package com.skillswap.skillswap.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "skills")
+public class Skill {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, nullable = false)
+	private String name;
+
+	public Skill() {
+	}
+
+	public Skill(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
